2a26ac029fa2e884818caf246f6d6f3e7ad7708e
rickrd <desouzaricardoc@gmail.com>
2019-12-17 23:05:15 -0300

    testing navigator geolocation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import Header from '../organisms/Header'
 
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition)
+  } else {
+    alert('Geolocation is not supported by this browser.')
+  }
+}
+
+const showPosition = position => {
+  alert('Latitude: ' + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude)
+}
+
 const Home = () => {
+  getLocation()
+
   return <Header></Header>
 }
 
