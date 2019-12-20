d3097254843b055332fa55dc9b72bc641a18be30
rickrd <desouzaricardoc@gmail.com>
2019-12-20 03:17:06 -0300

    created update weather data action

--- src/components/redux/actions.js
+++ src/components/redux/actions.js
@@ -1,5 +1,10 @@
 export const UPDATE_COORDINATES = 'UPDATE_COORDINATES'
+export const UPDATE_DATA = 'UPDATE_DATA'
 
 export const updateCoordinates = (lat, lon) => {
   return { type: UPDATE_COORDINATES, lat, lon }
+}
+
+export const updateData = data => {
+  return { type: UPDATE_DATA, data }
 }
