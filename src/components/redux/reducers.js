3e50bf20581bac07b2ad0ffa09a49c710117d23a
rickrd <desouzaricardoc@gmail.com>
2019-12-19 23:18:58 -0300

    made two coordinates update reducers into one that deals with the coordinates object values

--- src/components/redux/reducers.js
+++ src/components/redux/reducers.js
@@ -1,27 +1,24 @@
-import { UPDATE_LON, UPDATE_LAT } from './actions'
+import { UPDATE_COORDINATES } from './actions'
 
 const initialState = {
-  lat: 0,
-  lon: 0
+  coordinates: {
+    lat: 0,
+    lon: 0
+  }
 }
 
 const coordinates = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_LAT:
+    case UPDATE_COORDINATES:
       return Object.assign({}, state, {
-        lat: action.lat
-      })
-    case UPDATE_LON:
-      return Object.assign({}, state, {
-        lon: action.lon
+        coordinates: {
+          lat: action.lat,
+          lon: action.lon
+        }
       })
     default:
       return state
   }
 }
 
-export default function reducers(state = initialState, action) {
-  return {
-    coordinates: coordinates(state, action)
-  }
-}
+export default coordinates
