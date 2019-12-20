0d48ece8d0fdf515d62217fe7d1db71f182211e8
rickrd <desouzaricardoc@gmail.com>
2019-12-19 23:16:01 -0300

    moved navigator geolocation to SearchCoordinate component, added Provider and exported connected mapped state component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,35 +2,7 @@ import React from 'react'
 import Header from '../organisms/Header'
 import Body from '../organisms/Body'
 import styled from 'styled-components'
-
-const getLocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition, showError)
-  } else {
-    alert('Geolocation is not supported by this browser.')
-  }
-}
-
-const showPosition = position => {
-  alert('Latitude: ' + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude)
-}
-
-const showError = error => {
-  switch (error.code) {
-    case error.PERMISSION_DENIED:
-      alert('User denied the request for Geolocation.')
-      break
-    case error.POSITION_UNAVAILABLE:
-      alert('Location information is unavailable.')
-      break
-    case error.TIMEOUT:
-      alert('The request to get user location timed out.')
-      break
-    case error.UNKNOWN_ERROR:
-      alert('An unknown error occurred.')
-      break
-  }
-}
+import { Provider, connect } from 'react-redux'
 
 const HomeWrapper = styled.div`
   max-width: 1280px;
@@ -42,15 +14,22 @@ const HomeWrapper = styled.div`
   justify-content: center;
 `
 
-const Home = () => {
-  getLocation()
-
+const Home = props => {
+  const {store} = props
   return (
-    <HomeWrapper>
-      <Header></Header>
-      <Body></Body>
-    </HomeWrapper>
+    <Provider store={store}>
+      <HomeWrapper>
+        <Header></Header>
+        <Body></Body>
+      </HomeWrapper>
+    </Provider>
   )
 }
 
-export default Home
+function mapStateToProps(state) {
+  return {
+    coordinates: state.coordinates
+  }
+}
+
+export default connect(mapStateToProps)(Home)
