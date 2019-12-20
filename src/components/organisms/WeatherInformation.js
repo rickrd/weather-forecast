b075b392549053f029dca9bb174855f85ee070a6
rickrd <desouzaricardoc@gmail.com>
2019-12-20 03:18:26 -0300

    checking of theres any data key coming from state, if not try to get from browser

--- src/components/organisms/WeatherInformation.js
+++ src/components/organisms/WeatherInformation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import doRequest from '../../services/request'
+import { updateData } from '../redux/actions'
 
 const WeatherInformationWrapper = styled.div`
   display: flex;
@@ -14,18 +15,28 @@ const getData = async props => {
   const data = await doRequest(
     `https://api.openweathermap.org/data/2.5/weather?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&units=metric&APPID=01460cb31eb2c443498031402b438f94`
   )
-  console.log(data)
+  console.log(props)
+  props.dispatch(updateData(data))
   return data
 }
 
 const WeatherInformation = props => {
-  getData(props)
-  return <WeatherInformationWrapper>Results</WeatherInformationWrapper>
+  console.log(props)
+  const { data } = props
+  if (Object.keys(data).length === 0) {
+    getData(props)
+  }
+  return Object.keys(data).length !== 0 ? (
+    <WeatherInformationWrapper>{data.name}</WeatherInformationWrapper>
+  ) : (
+    <WeatherInformationWrapper>No results</WeatherInformationWrapper>
+  )
 }
 
 const mapStateToProps = state => {
   return {
-    coordinates: state.coordinates
+    coordinates: state.coordinates,
+    data: state.data
   }
 }
 
